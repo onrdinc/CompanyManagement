65fd809f57f696cd874a3513c9266ad78ad1622c
onurrdinc <onurdinc19@gmail.com>
2023-08-24 17:35:50 +0300

    last update

--- ToDoMvcUI/ToDo.MvcUI/Areas/Admin/Models/ResponseBody.cs
+++ ToDoMvcUI/ToDo.MvcUI/Areas/Admin/Models/ResponseBody.cs
@@ -5,6 +5,5 @@
         public T Data { get; set; }
         public List<string> ErrorMessages { get; set; }
         public int StatusCode { get; set; }
-        public int Count { get; set; } = 0;
     }
 }
